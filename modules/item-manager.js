c4d3e42ac9ea17a0e15a700dda51c0a3f050bdc1
MaNmAxIm0 <131485536+MaNmAxIm0@users.noreply.github.com>
2025-06-19 15:32:15 +0100

    Update item-manager.js

--- modules/item-manager.js
+++ modules/item-manager.js
@@ -1,4 +1,4 @@
-import { getItems, addItem, updateItem, deleteItem, updateItemOrder } from '../firebase.js';
+import { getItems, addItem, updateItem, deleteItem, updateItemOrder } from 'modules/firebase.js';
 
 export class ItemManager {
   constructor(currentTabId, uiUtils) {
